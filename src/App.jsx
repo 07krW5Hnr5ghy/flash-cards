f45ca1c1173044fadc776883b9200bdd08931318
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-01 23:45:27 -0500

    feature(main): import json data into app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import questionsData from './assets/questions.json'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FlashCard from './components/FlashCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  console.log(questionsData);
   return (
     <>
       <h1>Flash Cards</h1>
