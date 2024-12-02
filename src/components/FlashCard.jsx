a616ccd2f05852ece52720e3a83c75d94ae6d5d3
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-01 20:39:53 -0500

    feature(main): create flash card component

---
+++ src/components/FlashCard.jsx
@@ -0,0 +1,15 @@
+function FlashCard(){
+    return(
+        <div>
+            <div>
+                <p>Question / Answer</p>
+            </div>
+            <div>
+                <button>Previous</button>
+                <button>Show Answer</button>
+                <button>Next</button>
+            </div>
+        </div>
+    );
+}
+export default FlashCard;
