f2012b55c2683f91229e11f1f2345f96cf2741f1
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-03 19:56:02 -0500

    refactor(main): add css styling for flash card and progres bar components

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 function FlashCard({question,answer,setQuestion,lastIndex}){
     const [toggleAnswer,setToggleAnswer] = useState(false);
+    function previousQuestion(){
+        setQuestion(prev => prev>0 ? prev-1 : prev);
+        setToggleAnswer(false);
+    }
+    function nextQuestion(){
+        setQuestion(prev => prev<lastIndex?prev+1:prev);
+        setToggleAnswer(false);
+    }
     return(
         <div className="flash-card">
             <div className="flash-card-content">
                 <p>{toggleAnswer ? answer:question}</p>
             </div>
             <div className="flash-card-menu">
-                <button onClick={() => setQuestion(prev => prev>0 ? prev-1 : prev)}>Previous</button>
-                <button onClick={() => setToggleAnswer(!toggleAnswer)}>{toggleAnswer ? "Hide Answer" : "Show Answer"}</button>
-                <button onClick={() => setQuestion(prev => prev<lastIndex?prev+1:prev)}>Next</button>
+                <button className="button button-arrow" onClick={previousQuestion}>{"< Previous"}</button>
+                <button className="button main-button" onClick={() => setToggleAnswer(!toggleAnswer)}>{toggleAnswer ? "Hide Answer" : "Show Answer"}</button>
+                <button className="button button-arrow" onClick={nextQuestion}>{"Next >"}</button>
             </div>
         </div>
     );
